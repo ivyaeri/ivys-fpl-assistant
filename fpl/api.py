34306237ee6d4c14b49f21a830df9aff1e6d0457
ivyaeri <80895402+ivyaeri@users.noreply.github.com>
2025-08-12 19:09:49 +0100

    Create api.py

---
+++ fpl/api.py
@@ -0,0 +1,23 @@
+# fpl/api.py
+import requests
+from functools import lru_cache
+
+FPL_API = "https://fantasy.premierleague.com/api"
+
+@lru_cache(maxsize=8)
+def fetch_bootstrap():
+    r = requests.get(f"{FPL_API}/bootstrap-static/")
+    r.raise_for_status()
+    return r.json()
+
+@lru_cache(maxsize=8)
+def fetch_fixtures():
+    r = requests.get(f"{FPL_API}/fixtures/")
+    r.raise_for_status()
+    return r.json()
+
+@lru_cache(maxsize=512)
+def fetch_player_history(player_id: int):
+    r = requests.get(f"{FPL_API}/element-summary/{player_id}/")
+    r.raise_for_status()
+    return r.json()
